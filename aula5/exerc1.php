47df8767c339770d4de61a944d46968157b8feca
benhur <benhurcontel@gmail.com>
2022-03-23 11:26:16 -0300

    exerc1

--- aula5/exerc1.php
+++ aula5/exerc1.php
@@ -2,20 +2,21 @@
 $n1=1;
 $n2=2;
 $n3=3;
-    if($n1>$n2 and $n2>$n3)
-    echo $n1 " " $n2 " " $n3;
-    if($n1>$n2 and $n3>$n2)
-    echo $n1 " " $n3 " " $n2;
+    if($n1>$n2 && $n2>$n3)
+    echo $n1 " a" $n2 " a" $n3;
+    
+    if($n1>$n2 && $n3>$n2)
+    echo $n1 " a" $n3 "a " $n2;
 
-    if($n2>$n1 and $n1>$n3)
-    echo $n2 " " $n1 " " $n3;
-    if($n2>$n1 and $n3>$n1)
-    echo $n2 " " $n3 " " $n1;
+    if($n2>$n1 && $n1>$n3)
+    echo $n2 "a " $n1 " a" $n3;
+    if($n2>$n1 && $n3>$n1)
+    echo $n2 " a" $n3 " a" $n1;
 
-    if($n3>$n1 and $n2>$n1)
-    echo $n3 " " $n2 " " $n1;
-    if($n3>$n1 and $n1>$n2)
-    echo $n3 " " $n1 " " $n2;
+    if($n3>$n1 && $n2>$n1)
+    echo $n3 " a" $n2 " a" $n1;
+    if($n3>$n1 && $n1>$n2)
+    echo $n3 " a" $n1 " a" $n2;
     
 
 ?>
