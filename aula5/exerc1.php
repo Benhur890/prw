a3064361b370f827739a3270145b83e116658f09
benhur <benhurcontel@gmail.com>
2022-03-23 11:36:25 -0300

    exerc1

--- aula5/exerc1.php
+++ aula5/exerc1.php
@@ -12,12 +12,11 @@ $n3=3;
     echo $n2. $n1. $n3;
 
     elseif($n2>$n1 && $n3>$n1 && $n2>$n3)
-    echo $n2.  $n3."aa".  $n1;
+    echo $n2.  $n3.  $n1;
 
     elseif($n3>$n1 && $n2>$n1 && $n3>$n2)
     echo $n3.  $n2. $n1;
     elseif($n3>$n1 && $n1>$n2 && $n3>$n1)
     echo $n3.  $n1.  $n2;
     
-
 ?>
