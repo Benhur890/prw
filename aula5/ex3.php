201a4c0ed2e02de3617d2de62942d527cc0d9a71
benhur <benhurcontel@gmail.com>
2022-03-23 11:08:22 -0300

    ex3

---
+++ aula5/ex3.php
@@ -0,0 +1,10 @@
+<?php
+    $op = 'a';
+    switch($op){
+            case 'a':echo "Abrir"; break;
+            case 'f':echo "Fechar"; break;
+            case 's':echo "Salvar"; break;
+            case 'e':echo "Esperar"; break;
+            default:echo "Sair"; break;
+    }
+?>
