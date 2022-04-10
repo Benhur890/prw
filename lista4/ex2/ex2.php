05c86a965cc719c1857d37f349e967a50319efb3
Benhur <benhurcontel@gmail.com>
2022-04-10 16:38:30 -0300

    lista4

---
+++ lista4/ex2/ex2.php
@@ -0,0 +1,13 @@
+<?php
+echo "Caixa de texto de uma linha: ". $_GET["txtTexto"]. "<br>";
+
+echo "Caixa de texto de rolagem: ". $_GET["rolagem"]. "<br>";
+
+echo "Caixa de seleção: ". $_GET["checkbox"]. "<br>";
+
+
+echo "Botão de opção: ". $_GET["radio"]. "<br>";
+
+echo "Menu Suspenso: ". $_GET["lista"]. "<br>";
+
+?>
