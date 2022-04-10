1cea74c7af95497923ec6a26d7ec9b1eafc7250d
Benhur <benhurcontel@gmail.com>
2022-04-10 14:16:28 -0300

    lista

--- lista3/ex6.php
+++ lista3/ex6.php
@@ -1,11 +1,8 @@
 <?php
-$inversa = "oirartonc";
+$inversa = "oirartnoc";
 
-$partes = explode(".",$inversa);
-$carac = strlen($inversa);
-$cont= "4";
-$parte1 = $parte[0];
-    echo $partes[0];
-    $cont--;
+$carac = strrev($inversa);
+echo $inversa. "<br>";
+echo $carac;
 
 ?>
